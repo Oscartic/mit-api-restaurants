c21ad648df45d5158b57e9a3c8354e8359efdd58
Oscar Ballesteros <oscartic@gmail.com>
2022-02-12 19:24:47 -0300

    remove get dummy

--- server/app.js
+++ server/app.js
@@ -10,12 +10,6 @@ app.use(bodyParser.json());
 app.use(cors());
 routes.default(app);
 
-// respond with "hello world" when a GET request is made to the homepage
-app.get('/', function(req, res) {
-    console.log("Root app")
-    res.send('hello world');
-});
-
 app.listen(process.env.API_PORT, () => {
     console.info('API run in port: ', process.env.API_PORT);
 });
