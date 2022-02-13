de2c0e9d9b4520512b29185268ebd269eed87620
Oscar Ballesteros <oscartic@gmail.com>
2022-02-13 14:39:14 -0300

    add users list service

--- server/services/users-services/index.js
+++ server/services/users-services/index.js
@@ -2,10 +2,12 @@ const create = require('./users-actions/create.action');
 const get = require('./users-actions/get.action');
 const update = require('./users-actions/update.action');
 const remove = require('./users-actions/remove.action');
+const list = require('./users-actions/list.action');
 
 module.exports = {
     create,
     get,
     update,
     remove,
+    list
 };
