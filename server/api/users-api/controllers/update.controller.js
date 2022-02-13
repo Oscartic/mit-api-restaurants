584071402f8aa9563c9327fa1dd3263b3998d0a9
Oscar Ballesteros <oscartic@gmail.com>
2022-02-13 13:48:47 -0300

    add routes and user update controller

---
+++ server/api/users-api/controllers/update.controller.js
@@ -0,0 +1,26 @@
+const Joi = require('Joi');
+const UsersService = require('../../../services/users-services');
+const CreateUserValidationSchema = require('../schemas/update.schema');
+
+const schema = Joi.object().keys(CreateUserValidationSchema);
+
+module.exports = async function update (req, res) {
+    
+    const { id: userId } = req.params;
+    const { body } = req;
+    
+    try {
+        const bodyValues = await schema.validateAsync(body);
+        const { name, nickname, password } = bodyValues;
+        const response  = await UsersService.update({
+            userId, name, nickname, password,
+        });
+        console.log(response)
+        if(response.error) return res.status(400).send({ response });
+        return res.status(201).send({ response });
+        
+    } catch (error) {
+        console.log('[update.controller] >>> ', error);
+        return res.status(500).send({error: 'Internal Server Error'});
+    }
+}
--- server/api/users-api/routes.js
+++ server/api/users-api/routes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const create = require('./controllers/create.controller');
 const get = require('./controllers/get.controller');
+const update = require('./controllers/update.controller');
 
 const router = express.Router();
 
 router.post('/', create);
 router.get('/:id', get);
-// router.put('/:id', isJwtAuthenticated, update);
+router.put('/:id', update);
 // router.get('/', isJwtAuthenticated, list);
 // router.delete('/:id', isJwtAuthenticated, remove);
 
