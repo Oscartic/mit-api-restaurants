ae24bbdec3292fd4888084f7c1c45dd172f56ae7
Oscar Ballesteros <oscartic@gmail.com>
2022-02-15 09:42:43 -0300

    add XSRF_TOKEN in routes

--- server/api/routes.js
+++ server/api/routes.js
@@ -2,6 +2,11 @@ const apiUsers = require('./users-api/routes');
 
 module.exports.default = (app) => {
 
+    app.all('*', (req, res, next) => {
+        res.cookie("XSRF_TOKEN", req.csrfToken());
+        next();
+    });
+
     app.use('/api/users', apiUsers);
 
     app.use('/api/status', (req, res) => {
@@ -9,4 +14,5 @@ module.exports.default = (app) => {
         res.send({ message: 'Mit Api Restaurants works and it status is online 🔛' });
         res.end();
     });
+
 };
