13d22dafcb9fe51bf8a646e7b580071b703332a1
Oscar Ballesteros <oscartic@gmail.com>
2022-02-15 09:39:36 -0300

    include firebase config server

--- server/app.js
+++ server/app.js
@@ -1,17 +1,34 @@
 const express = require('express');
 const routes = require('./api/routes');
+const cookieParser = require('cookie-parser');
+const csrf = require("csurf");
 const bodyParser = require('body-parser');
 const db = require('../config/mongodb');
 const cors = require('cors');
+const admin = require("firebase-admin");
+
+const serviceAccount = require("../config/serviceAccountKey.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+const csrfMiddleware = csrf({ cookie: true});
+
+const PORT = process.env.API_PORT || 3001;
 
 db.connect();
+
 const app = express();
 app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(csrfMiddleware);
 app.use(cors());
+
 routes.default(app);
 
-app.listen(process.env.API_PORT, () => {
-    console.info('API run in port: ', process.env.API_PORT);
+app.listen(PORT, () => {
+    console.info('API run in port: ', PORT);
 });
 
 module.exports = app;
