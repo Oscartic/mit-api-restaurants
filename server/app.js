f7a4b12156299fc4777a97b9fce62dbeb821d01c
Oscar Ballesteros <oscartic@gmail.com>
2022-02-22 19:03:23 -0300

    set environment var port

--- server/app.js
+++ server/app.js
@@ -4,14 +4,16 @@ const bodyParser = require('body-parser');
 const db = require('../config/mongodb');
 const cors = require('cors');
 
+const PORT = process.env.API_PORT;
+
 db.connect();
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 routes.default(app);
 
-app.listen(process.env.API_PORT, () => {
-    console.info('API run in port: ', process.env.API_PORT);
+app.listen(PORT, () => {
+    console.info('API run in port: ', PORT);
 });
 
 module.exports = app;
