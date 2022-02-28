57e1240fcb0a6a3a88cd9eec262dd7903e9cf0fd
Oscar Ballesteros <oscartic@gmail.com>
2022-02-27 23:39:40 -0300

    fix string key firebase

--- config/serviceAccountKey.js
+++ config/serviceAccountKey.js
@@ -3,7 +3,7 @@ module.exports = {
     type: process.env.FIREBASE_ADMIN_TYPE,
     project_id: process.env.FIREBASE_ADMIN_PROJECT_ID,
     private_key_id: process.env.FIREBASE_ADMIN_PRIVATE_KEY_ID,
-    private_key: process.env.FIREBASE_ADMIN_PRIVATE_KEY,
+    private_key: process.env.FIREBASE_ADMIN_PRIVATE_KEY.replace(/\\n/g, '\n'),
     client_email: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
     client_id: process.env.FIREBASE_ADMIN_CLIENT_ID,
     auth_uri: process.env.FIREBASE_ADMIN_AUTH_URI,
